60f7e81c10f1a81f431d8b13751d2a6da548ea2d
GongSoSn <renjianyong0923@outlook.com>
2016-07-20 17:39:02 +0800

    Mod By Ren,Jianyong

--- day02/src/com/day02/cn/test.java
+++ day02/src/com/day02/cn/test.java
@@ -3,5 +3,6 @@ package com.day02.cn;
 public class test {
     public static void main(String[] args) {
         System.out.println("This is a first program!");
+        System.out.println("My Repositry !");
     }
 }
