077fb6ed5ebe6b904c4f6ad3258f50244763d21c
GongSoSn <renjianyong0923@outlook.com>
2016-07-21 11:17:05 +0800

    Mod By Ren,Jianyong || 02 Operators(Logical,count)

--- day03/src/com/day03/cn/logicalOperators.java
+++ day03/src/com/day03/cn/logicalOperators.java
@@ -18,5 +18,38 @@ public class logicalOperators {
 
         boolean test04 = !test ^ test;
         System.out.println("test04 : " + test04);
+
+        /*
+         * Logical And (&), Logical or(|)
+         */
+
+        System.out.println("Logical And (&), Logical or(|)");
+
+        System.out.println("L_And : " + (true & false));// false
+        System.out.println("L_Or : " + (true | false));// true
+
+        System.out.println("Short Logical And,or !");
+
+        System.out.println("Short And : " + ((1 > 2) && (1 < 2)));// false
+        System.out.println("Short or : " + (1 < 2 || 2 < 3));// true
+
+        char c = 'A';
+        // System.out.println(c);
+        char ch = (char) (c + 1);
+        System.out.println(ch);
+
+        double a1 = 12.345678;
+
+        int a2 = (int) (a1 * 100 + 0.5);// 1235.0678
+        System.out.println((a1 * 100 + 0.5));
+        System.out.println((int) (123.6789));
+        System.out.println(a2);
+        double a3 = (double) a2 / 100;// 1235.0/100
+        System.out.println((double) a2);
+        System.out.println(a3);
+        double hh = 12.0000000000000000;
+        System.out.println(hh);
+        System.out.println(((double) 1235) / 100);
+        int ￥ii = 9;// This is a legal identifier;
     }
 }
