9bd332f2d03dda8f90c4504e591ed5b453210b39
GongSoSn <renjianyong0923@outlook.com>
2016-07-27 17:41:01 +0800

    parameter test Add By Ren,Jianyong

---
+++ day07/src/com/OOP1/cn/ParaTest.java
@@ -0,0 +1,11 @@
+package com.OOP1.cn;
+
+public class ParaTest {
+    public static void say(String s) {
+        System.out.println("我想说 ： " + s);
+    }
+
+    public static void main(String[] args) {
+        say("12秒88 ！");
+    }
+}
